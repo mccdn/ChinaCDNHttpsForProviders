560215a7d47db267f1de05be1b9025628a80cd2c
Paul <cuidianpeng@llive.cn>
2018-08-07 12:06:55 +0800

    Add comments for .net version

--- ReadSSLCertFromKeyVault-Net/ReadSSLCert/Program.cs
+++ ReadSSLCertFromKeyVault-Net/ReadSSLCert/Program.cs
@@ -14,17 +14,62 @@
     {
         static void Main(string[] args)
         {
-            string CredentialKeyVaultAccessCertThumbprint = ConfigurationManager.AppSettings["CredentialKeyVaultAccessCertThumbprint"];
-            string CredentialKeyVaultAuthClientId = ConfigurationManager.AppSettings["CredentialKeyVaultAuthClientId"];
-            string sslCertUri = ConfigurationManager.AppSettings["sslCertUri"];
+            //此字段为AAD证书的Thumbprint，用来查找本地安装证书并初始化密钥保管库。
+            string credentialKeyVaultAccessCertThumbprint = ConfigurationManager.AppSettings["CredentialKeyVaultAccessCertThumbprint"];
 
-            var keyVaultClient = InitWithCert(CredentialKeyVaultAuthClientId, CredentialKeyVaultAccessCertThumbprint);
-            Run(keyVaultClient, sslCertUri);
+            //此字段为AAD的ClientId，取自BindCert Api的ClientID
+            string credentialKeyVaultAuthClientId;
+
+            //此字段为证书的全路径，取自BindCert Api的Certificate字段
+            string sslCertUri;
+
+            //此字段为供应商端的Provider Id，，取自BindCert Api的Certificate字段
+            string endpointId;
+
+            //从BindCert HttpRequest中获取参数。
+            ReadParamtersFromBindCertHttpRequest(out credentialKeyVaultAuthClientId, out sslCertUri, out endpointId);
+
+            //通过本地AAD证书初始化密钥保管库的客户端
+            var keyVaultClient = InitKeyVaultWithAADCert(credentialKeyVaultAuthClientId, credentialKeyVaultAccessCertThumbprint);
+
+            //通过密钥保管库客户端获取证书并保存到本地。
+            GetAndDownLoadSSLCert(keyVaultClient, sslCertUri);
 
             Console.Read();
         }
 
-        static async void Run(KeyVaultClient keyVaultClient, string sslCertUri)
+        /// <summary>
+        /// 从BindCert Api的http request中获取参数
+        /// </summary>
+        /// <param name="authClientId">此字段为AAD的ClientId，实际应取自BindCert Api的ClientID</param>
+        /// <param name="sslCertUri">此字段为证书的全路径，适应应取自BindCert Api的Certificate字段</param>
+        /// <param name="endpointId">供应商端的Endpoint Id.适应应取自BindCert Api的EndpointId字段</param>
+        static void ReadParamtersFromBindCertHttpRequest(out string authClientId, out string sslCertUri, out string endpointId)
+        {
+            ///// 以下为Azure CDN Service 调用BindCert 传输的参数示例：
+            ////{ 
+            ////“EndpointId”: “cc9706ec-7f99-4df9-83a9-4820931a2552”, 
+            ////“Certificate”: “https://transit-cdn-cert.vault.azure.cn/certificates/cc9706ec-7f99-4df9-83a9-4820931a2552” 
+            ////“ClientID”: “5451E51E-7E8D481C-BD44-41E25B580F26” 
+            ////}
+
+            //此字段为AAD的ClientId，实际应取自BindCert Api的ClientID
+            authClientId = ConfigurationManager.AppSettings["CredentialKeyVaultAuthClientId"];
+
+            //此字段为证书的全路径，实际应取自BindCert Api的Certificate字段
+            sslCertUri = ConfigurationManager.AppSettings["sslCertUri"];
+
+            //供应商端的Endpoint Id.实际应取自BindCert Api的EndpointId字段
+            endpointId = Guid.NewGuid().ToString();
+        }
+
+
+        /// <summary>
+        /// 获取并下载SSL证书
+        /// </summary>
+        /// <param name="keyVaultClient">用来访问密钥保管库的客户端</param>
+        /// <param name="sslCertUri">SSL Cert的Uri路径，此路径从BindCert的参数Certificate中获取到</param>
+        static async void GetAndDownLoadSSLCert(KeyVaultClient keyVaultClient, string sslCertUri)
         {
             var certContentSecret = await keyVaultClient.GetSecretAsync(sslCertUri).ConfigureAwait(false);
 
@@ -33,7 +78,7 @@
 
             if (string.CompareOrdinal(certContentSecret.ContentType, CertificateContentType.Pem) == 0)
             {
-                //Pem File.
+                //Pem文件.
                 using (FileStream fs = new FileStream($"{Guid.NewGuid()}.pem", FileMode.Create))
                 {
                     byte[] content = Encoding.UTF8.GetBytes(certContentSecret.Value);
@@ -43,6 +88,7 @@
             }
             else if (string.CompareOrdinal(certContentSecret.ContentType, CertificateContentType.Pfx) == 0)
             {
+                //PFX 文件
                 using (FileStream fs = new FileStream($"{Guid.NewGuid()}.pfx", FileMode.Create))
                 {
                     byte[] content = Convert.FromBase64String(certContentSecret.Value);
@@ -56,25 +102,35 @@
             }
         }
 
-        static private KeyVaultClient InitWithCert(string authClientId, string authThumbprint)
+        /// <summary>
+        /// 通过本地AAD证书初始化密钥保管库的客户端
+        /// </summary>
+        /// <param name="authClientId">此字段为AAD的ClientId，实际应取自BindCert Api的ClientID</param>
+        /// <param name="authThumbprint">此字段为AAD证书的Thumbprint，用来查找本地安装证书并初始化密钥保管库。</param>
+        /// <returns>获取密钥保管库的客户端。</returns>
+        static private KeyVaultClient InitKeyVaultWithAADCert(string authClientId, string authThumbprint)
         {
             X509Certificate2 cert = GetCertificateByThumbprint(authThumbprint, StoreName.My, StoreLocation.LocalMachine);
 
             ClientAssertionCertificate assertionCert = new ClientAssertionCertificate(authClientId, cert);
 
             KeyVaultClient client = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(
-                                       (authority, resource, scope)
-                                           => GetAccessTokenWithCert(authority, resource, scope, assertionCert)));
+                                       async (authority, resource, scope) =>
+                                       {
+                                           AuthenticationContext context = new AuthenticationContext(authority, TokenCache.DefaultShared);
+                                           AuthenticationResult result = await context.AcquireTokenAsync(resource, assertionCert).ConfigureAwait(false);
+                                           return result.AccessToken;
+                                       }));
             return client;
         }
 
-        static async Task<string> GetAccessTokenWithCert(string authority, string resource, string scope, ClientAssertionCertificate assertionCert)
-        {
-            AuthenticationContext context = new AuthenticationContext(authority, TokenCache.DefaultShared);
-            AuthenticationResult result = await context.AcquireTokenAsync(resource, assertionCert).ConfigureAwait(false);
-            return result.AccessToken;
-        }
-
+        /// <summary>
+        /// 获取本地已安装证书
+        /// </summary>
+        /// <param name="thumbprint">证书的thumbprint.</param>
+        /// <param name="name">证书存储的名称</param>
+        /// <param name="location">证书存储的地址（Localmachine/CurrentUser）</param>
+        /// <returns>返回X509证书</returns>
         static X509Certificate2 GetCertificateByThumbprint(string thumbprint, StoreName name, StoreLocation location)
         {
             var certStore = new X509Store(name, location);
